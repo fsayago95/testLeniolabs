e88938ab8d9ae7cb37b2cf8cc9986103139da3ec
Franco Sayago <sayago.fran@gmail.com>
2021-07-28 12:54:36 -0500

    delete files

--- test/specs/example2.js
+++
@@ -1,9 +0,0 @@
-import Home from '../pageobjects/home'
-
-describe('Example', () => {
-    it('Example 2', async () => {
-        Home.open("https://www.mercadolibre.com.ar/")
-        Home.searchMiscellaneous()
-        Home.clickOnSearch()
-    })
-})
